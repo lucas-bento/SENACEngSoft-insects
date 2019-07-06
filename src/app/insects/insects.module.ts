e4c6bc54df9cbbc77fa1c35a1bb5fc3c8adb7430
Clayton Chagas <Clayton.Chagas@senacrjedu.local>
2019-07-06 16:41:11 -0300

    Initial commit

---
+++ src/app/insects/insects.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InsectsRoutingModule } from './insects-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InsectsRoutingModule
+  ]
+})
+export class InsectsModule { }
